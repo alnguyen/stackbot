3eb09897a5cb33c493ccde40cff56464781ea6b5
Alan Nguyen <nguyen.alan@me.com>
2016-02-25 14:13:23 -0700

    begin to move repo to standardjs, ensure tests work

--- tests/Slack_web_api.js
+++ tests/Slack_web_api.js
@@ -1,104 +1,105 @@
-var should = require('should');
-var Botkit = require('../');
-var path = require('path');
-var tmpdir = require('os').tmpdir();
-var fs = require('fs');
-var winston = require('winston');
+var should = require('should')
+var Botkit = require('../')
+var path = require('path')
+var tmpdir = require('os').tmpdir()
+var fs = require('fs')
+var winston = require('winston')
+require('envc')({})
 
-var token = process.env.TOKEN;
+var token = process.env.token
 
-describe('Test', function() {
-    it('should have a token', function(done) {
-        should.exist(token);
-        done();
-    });
+describe('Test', function () {
+  it('should have a token', function (done) {
+    should.exist(token)
+    done()
+  })
 
-    it('should have Botkit instance', function(done) {
-        should.exist(Botkit);
-        should.exist(Botkit.core);
-        should.exist(Botkit.slackbot);
-        done();
-    });
-});
+  it('should have Botkit instance', function (done) {
+    should.exist(Botkit)
+    should.exist(Botkit.core)
+    should.exist(Botkit.slackbot)
+    done()
+  })
+})
 
-describe('Botkit', function() {
-    this.timeout(5000);
+describe('Botkit', function () {
+  this.timeout(5000)
 
-    it('should start and then stop', function(done) {
-        var controller = Botkit.slackbot({debug: false});
-        var openIsCalled = false;
+  it('should start and then stop', function (done) {
+    var controller = Botkit.slackbot({debug: false})
+    var openIsCalled = false
 
-        controller.on('rtm_open', function(bot) {
-            should.exist(bot);
-            openIsCalled = true;
-        });
+    controller.on('rtm_open', function (bot) {
+      should.exist(bot)
+      openIsCalled = true
+    })
 
-        controller.on('rtm_close', function(bot) {
-            should.exist(bot);
-            openIsCalled.should.be.true;
-            controller.shutdown();
-            done();
-        });
+    controller.on('rtm_close', function (bot) {
+      should.exist(bot)
+      openIsCalled.should.be.true
+      controller.shutdown()
+      done()
+    })
 
-        controller
-            .spawn({
-                token: token
-            })
-            .startRTM(function(err, bot, payload) {
-                (err === null).should.be.true;
-                should.exist(bot);
-                bot.closeRTM();
-            });
-    });
+    controller
+      .spawn({
+        token: token
+      })
+      .startRTM(function (err, bot, payload) {
+        (err === null).should.be.true
+        should.exist(bot)
+        bot.closeRTM()
+      })
+  })
 
-    it('should have fail with false token', function(done) {
-        this.timeout(5000);
+  it('should have fail with false token', function (done) {
+    this.timeout(5000)
 
-        var controller = Botkit.slackbot({debug: false});
+    var controller = Botkit.slackbot({debug: false})
 
-        controller
-            .spawn({
-                token: '1234'
-            })
-            .startRTM(function(err, bot, payload) {
-                should.exist(err);
+    controller
+      .spawn({
+        token: '1234'
+      })
+      .startRTM(function (err, bot, payload) {
+        should.exist(err)
 
-                controller.shutdown();
-                done();
-            });
-    });
-});
+        controller.shutdown()
+        done()
+      })
+  })
+})
 
-describe('Log', function() {
-    it('should use an external logging provider', function(done) {
-        var logFile = path.join(tmpdir, 'botkit.log');
-        var logger = new winston.Logger({
-            transports: [
-                new (winston.transports.File)({ filename: logFile })
-            ]
-        });
+describe('Log', function () {
+  it('should use an external logging provider', function (done) {
+    var logFile = path.join(tmpdir, 'botkit.log')
+    var logger = new winston.Logger({
+      transports: [
+        new (winston.transports.File)({ filename: logFile })
+      ]
+    })
 
-        logger.cli();
+    logger.cli()
 
-        var controller = Botkit.slackbot({
-                debug: true,
-                logger: logger
-            });
+    var controller = Botkit.slackbot({
+      debug: true,
+      logger: logger
+    })
 
-        controller
-            .spawn({
-                token: '1234'
-            })
-            .startRTM(function(err, bot, payload) {
-                should.exist(err);
+    controller
+      .spawn({
+        token: '1234'
+      })
+      .startRTM(function (err, bot, payload) {
+        should.exist(err)
 
-                controller.shutdown();
+        controller.shutdown()
 
-                fs.readFile(logFile, 'utf8', function(err, res) {
-                    (err === null).should.be.true;
-                    should.exist(res);
-                    done();
-                });
-            });
-    });
-});
+        fs.readFile(logFile, 'utf8', function (err, res) {
+          (err === null).should.be.true
+          should.exist(res)
+          done()
+        })
+      })
+  })
+})
