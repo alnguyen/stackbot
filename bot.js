c1e86e96d3f58a097a33f3e3cc2bfdbb12a83f8f
Alan Nguyen <nguyen.alan@me.com>
2016-02-29 13:17:18 -0700

    remove shutdown command

--- bot.js
+++ bot.js
@@ -135,32 +135,6 @@ controller.hears(['call me (.*)'], constants.ADDRESSED, function (bot, message) 
   })
 })
 
-controller.hears(['shutdown'], constants.ADDRESSED, function (bot, message) {
-  bot.startConversation(message, function (err, convo) {
-    logError(err, 'Failed to start conversation')
-    convo.ask('Are you sure you want me to shutdown?', [
-      {
-        pattern: bot.utterances.yes,
-        callback: function (response, convo) {
-          convo.say('Bye!')
-          convo.next()
-          setTimeout(function () {
-            process.exit()
-          }, 3000)
-        }
-      },
-      {
-        pattern: bot.utterances.no,
-        default: true,
-        callback: function (response, convo) {
-          convo.say('*Phew!*')
-          convo.next()
-        }
-      }
-    ])
-  })
-})
-
 controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], constants.ADDRESSED, function (bot, message) {
   var hostname = os.hostname()
   var uptime = formatUptime(process.uptime())
