8daa14e3871bbc3596597f5b582f142d1cd75d5e
Alan Nguyen <nguyen.alan@me.com>
2016-03-01 19:52:40 -0700

    remove lame commands

--- bot.js
+++ bot.js
@@ -107,47 +107,7 @@ controller.hears([constants.LOOKUP], constants.ADDRESSED, function (bot, message
   }
 })
 
-controller.hears(['hello', 'hi'], constants.ADDRESSED, function (bot, message) {
-  bot.api.reactions.add({
-    timestamp: message.ts,
-    channel: message.channel,
-    name: 'robot_face'
-  }, function (err, res) {
-    if (err) {
-      bot.botkit.log('Failed to add emoji reaction :(', err)
-    }
-  })
-
-  controller.storage.users.get(message.user, function (err, user) {
-    logError(err, 'Failed to get user')
-
-    if (user && user.name) {
-      bot.reply(message, 'Hello ' + user.name + '!!')
-    } else {
-      bot.reply(message, 'Hello.')
-    }
-  })
-})
-
-controller.hears(['call me (.*)'], constants.ADDRESSED, function (bot, message) {
-  var matches = message.text.match(/call me (.*)/i)
-  var name = matches[1]
-  controller.storage.users.get(message.user, function (err, user) {
-    logError(err, 'Failed to get user')
-    if (!user) {
-      user = {
-        id: message.user
-      }
-    }
-    user.name = name
-    controller.storage.users.save(user, function (err, id) {
-      logError(err, 'Failed to save user')
-      bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.')
-    })
-  })
-})
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], constants.ADDRESSED, function (bot, message) {
+controller.hears(['uptime'], constants.ADDRESSED, function (bot, message) {
   var hostname = os.hostname()
   var uptime = formatUptime(process.uptime())
 
