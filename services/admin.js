d06c1e1dab5d48935722203a7507349cbecbe6c6
Alan Nguyen <nguyen.alan@me.com>
2016-03-13 13:25:41 -0600

    validate user admin status, call appropriate function

--- services/admin.js
+++ services/admin.js
@@ -1,4 +1,20 @@
 var constants = require('../constants')
+var slackWebApi = require('../lib/Slack_web_api.js')
+
+function disableService (service) {
+  // Disables Service
+  console.log(`disable ${service}`)
+}
+
+function enableService (service) {
+  // Enables Service
+  console.log(`enable ${service}`)
+}
+
+var accessMethods = {
+  disable: disableService,
+  enable: enableService
+}
 
 function logError (bot, err, msg) {
   if (err) {
@@ -8,5 +24,15 @@ function logError (bot, err, msg) {
 }
 
 module.exports = function (bot, message, cb) {
+  var text = message.text
+  var action = text.substr(0, text.indexOf(' '))
+  var service = text.substr(text.indexOf(' ') + 1)
+  var user = message.user
+
+  bot.api.users.info({user}, (err, res) => {
+    if (res.user && res.user.is_admin) {
+      accessMethods[action](service)
+    }
+  })
 
 }
